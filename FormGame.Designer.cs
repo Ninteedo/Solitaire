168616e92d63b1815a13f17d2190db1508ead8ed
Richard Holmes <richard.gordon.holmes@gmail.com>
2020-08-24 12:28:42 +0100

    Renamed main form and set background colour to green

--- FormGame.Designer.cs
+++ FormGame.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Solitaire
 {
-    partial class Form1
+    partial class FormGame
     {
         /// <summary>
         /// Required designer variable.
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(1409, 694);
+            this.Name = "FormGame";
+            this.Text = "FormGame";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- FormGame.cs
+++ FormGame.cs
@@ -8,13 +8,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+#nullable enable
+
 namespace Solitaire
 {
-    public partial class Form1 : Form
+    public partial class FormGame : Form
     {
-        public Form1()
+        public FormGame()
         {
             InitializeComponent();
         }
+
+        #region "User Interface"
+
+        
+
+        #endregion
     }
 }
